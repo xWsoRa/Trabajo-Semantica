13fe19b7bcd70f39139d601763f2a5dd42afb797
xWsoRa <130982325+xWsoRa@users.noreply.github.com>
2023-04-17 21:05:24 +0200

    Update script_menu.js

--- script_menu.js
+++ script_menu.js
@@ -1,10 +1,10 @@
 const gameButton1 = document.getElementById("gameButton1");
 gameButton1.addEventListener("click", function() {
-  window.open("file:///C:/Users/Pilar/Desktop/Uni%20mario/curso%204/semantica/game%20from%20internet/index.html", "_blank");
+  window.open("https://xwsora.github.io/Multiple-choice/", "_blank");
 });
 const gameButton2 = document.getElementById("gameButton2");
 gameButton2.addEventListener("click", function() {
-  window.open("file:///C:/Users/Pilar/Desktop/Uni%20mario/curso%204/semantica/game%20from%20internet/fill_gap.html", "_blank");
+  window.open("https://xwsora.github.io/Fill_Blank/fill_gap", "_blank");
 });
 const gameButton3 = document.getElementById("gameButton3");
 gameButton3.addEventListener("click", function() {
